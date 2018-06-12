6053f2b9bf8dc40bc15d63153c1f4e625a0e0e62
vdrmota <vojtech_drmota@college.harvard.edu>
2018-06-12 15:44:49 +0200

    broadcasting added

--- broadcast.js
+++ broadcast.js
@@ -17,14 +17,14 @@ module.exports = {
 
 	blockchain: function(blockchainFile)
 	{
-		/*var blockchain = fs.readFileSync(blockchainFile).toString()
+		var blockchain = fs.readFileSync(blockchainFile).toString()
 		blockchain = blockchain.replace(/\+/g, "%2B"); 
 		var res = request('POST', blockchainServer, {
 			headers: {       
     			'content-type': 'application/x-www-form-urlencoded'
   			},
 			body: "blockchain="+encodeURIComponent(blockchain)
-		})*/
+		})
 		console.log("Broadcasting blockchain...")
 	},
 
@@ -33,19 +33,7 @@ module.exports = {
 	transaction: function(type, from, to, amount, chainHash, timestamp, mempoolFile)
 	{
 		// right now this broadcasts to local mempool, but it should broadcast to actual mempool in the future
-		var transaction = new Transaction(type, from, to, amount, chainHash, timestamp)
-		var current = fs.readFileSync(mempoolFile)
-		if (current != "")
-		{
-			current = JSON.parse(current)
-			current.push(transaction)
-		}
-		else
-		{
-			current = [JSON.stringify(transaction)]
-		}
-		fs.writeFileSync(mempoolFile, JSON.stringify(current))
-		/*signature = signature.replace(/\+/g, "%2B")
+		signature = signature.replace(/\+/g, "%2B")
 		origin = origin.replace(/\+/g, "%2B")
 		var transaction = JSON.stringify(new Transaction(type, from, to, amount, chainHash, timestamp))
 		var res = request('POST', transactionServer, {
@@ -53,7 +41,7 @@ module.exports = {
     			'content-type': 'application/x-www-form-urlencoded'
   			},
 			body: "transaction="+encodeURIComponent(transaction)
-		})*/
+		})
 		console.log("Broadcasting transaction...")
 	}
 }
---
+++ server/server.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function (socket){
+
+	console.log('New connection.');
+
+	// when receiving an updated blockchain, emit it to all nodes
+	socket.on('emit_blockchain', function (blockchain) {
+		// emit to all nodes
+		io.sockets.emit('receive_blockchain', blockchain[0])
+		console.log("Emitting new state of blockchain...")
+	});
+
+	// when receiving a new transaction to be added to mempools, emit it to all nodes
+	socket.on('emit_transaction', function (transaction) {
+		// emit to all nodes
+		io.sockets.emit('receive_transaction', transaction[0])
+		console.log("Emitting new transaction...")
+	});
+
+});
+
+http.listen(1338, function () {
+  console.log('listening on *:1338');
+});
