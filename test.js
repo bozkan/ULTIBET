364173631567ac83ab7bab90fa4bb70fa4f1119e
vdrmota <vojtech_drmota@college.harvard.edu>
2018-07-30 14:31:29 +0200

    cleanup

--- test.js
+++
@@ -1,6 +0,0 @@
-// var blockchain = require('./blockchain')
-
-// console.log(blockchain.findPayouts(6, "Poland", 47439))
-var helpers = require('./functions.js')
-
-console.log(helpers.ipToFlag('61.68.132.19').flag)
